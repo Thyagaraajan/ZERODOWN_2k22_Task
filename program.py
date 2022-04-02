0003301f8872a76f4743fb8c7771f3a8816fd6cf
Thyagaraajan <80024953+Thyagaraajan@users.noreply.github.com>
2022-04-02 15:07:51 +0530

    Add files via upload

---
+++ program.py
@@ -0,0 +1,78 @@
+import pymongo
+import numpy as np
+from matplotlib import pyplot as plt
+#%matplotlib inline 
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["local"]
+def dictFunction(val):
+    mycol = mydb[val]
+
+    myquery = [{
+        "$group" : 
+            {"_id" : ["$ZIP","$CITY","$STATE"],
+            "numb" : {"$sum" : 1}
+            }}
+        ]
+
+    mydoc = mycol.aggregate(myquery)
+    D = dict()
+    for i in mydoc:
+        D[tuple(i["_id"])] = i["numb"]
+
+    return D
+L = ["All_places_of_worship","Convention_Centers_Fairgrounds","Cruise_Line_Terminals","Major_Sport_Venues","Mobile_Home_Parks"]
+new_Dict = dict()
+
+for i in range(len(L)):
+    d = dictFunction(L[i])
+    for j in d:
+        if j[0] not in new_Dict:
+            l = [j[1],j[2],j[0],0,0,0,0,0]
+            l[3+i] = d[j]
+            new_Dict[j[0]] = l
+        else:
+            s = new_Dict[j[0]]
+            s[3+i] = d[j]
+            new_Dict[j[0]] = s
+
+#print(new_Dict)
+
+key_list = np.array([i for i in new_Dict.keys()])
+worship_list = np.array([i[3] for i in new_Dict.values()])
+center_list = np.array([i[4] for i in new_Dict.values()])
+cruise_list = np.array([i[5] for i in new_Dict.values()])
+sport_list = np.array([i[6] for i in new_Dict.values()])
+mob_home_list = np.array([i[7] for i in new_Dict.values()])
+
+plt.subplot(1,2,1)
+plt.plot(key_list,worship_list)
+plt.title("Worship Plot")
+plt.ylabel("Places Of Worship")
+plt.xlabel("ZIP Code")
+
+plt.subplot(1,2,2)
+plt.plot(key_list,center_list)
+plt.title("Convention Center Plot")
+plt.ylabel("Convention Centers")
+plt.xlabel("ZIP Code")
+
+plt.subplot(2,2,1)
+plt.plot(key_list,cruise_list)
+plt.title("Cruise Terminals Plot")
+plt.ylabel("Cruise Terminals")
+plt.xlabel("ZIP Code")
+
+plt.subplot(2,2,2)
+plt.plot(key_list,sport_list)
+plt.title("Sport Venues Plot")
+plt.ylabel("Sport Venues")
+plt.xlabel("ZIP Code")
+
+plt.subplot(3,1,1)
+plt.plot(key_list,mob_home_list)
+plt.title("Mobile Homes Plot")
+plt.ylabel("Mobile Homes")
+plt.xlabel("ZIP Code")
+
+plt.show()
